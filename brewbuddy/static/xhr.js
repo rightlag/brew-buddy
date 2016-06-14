b29228c162c9c1d4c9d0c6592370a8a8b450b8f4
rightlag <rightlag@gmail.com>
2016-06-14 19:37:55 +0000

    xhr functionality+template inheritance

--- brewbuddy/static/xhr.js
+++ brewbuddy/static/xhr.js
@@ -1,13 +1,16 @@
 var autocomplete;
+var input;
 function initAutocomplete() {
-  var input = document.getElementById('autocomplete');
+  input = document.getElementById('autocomplete');
   autocomplete = new google.maps.places.Autocomplete(input);
 }
 function xhr(event) {
   event.preventDefault();
+  // Clear the input field.
+  input.value = '';
   var place = autocomplete.getPlace();
   var data = {
-    name: place.name,
+    title: place.name,
     lat: place.geometry.location.lat(),
     lng: place.geometry.location.lng()
   };
@@ -15,5 +18,29 @@ function xhr(event) {
     contentType: 'application/json',
     data: JSON.stringify(data),
     type: 'POST'
+  });
+  xhttp.done(function (data) {
+    var table = document.getElementById('features');
+    // By default, assume the item is not found in the array.
+    var found = false;
+    for (var i = 0; i < table.rows.length; i++) {
+      if (data.properties.title === table.rows[i].cells[0].innerHTML) {
+        found = true;
+        table.rows[i].cells[1].innerHTML++;
+        break;
+      }
+    }
+    if (!found) {
+      var tbody = table.getElementsByTagName('tbody')[0];
+      var row = tbody.insertRow(0);
+      var c1 = row.insertCell(0);
+      var c2 = row.insertCell(1);
+      c1.innerHTML = data.properties.title;
+      c1.className = 'mdl-data-table__cell--non-numeric';
+      c2.innerHTML = data.properties.description;
+    }
+  });
+  xhttp.fail(function (jqXHR) {
+    console.log(jqXHR);
   });
 }
--- brewbuddy/views.py
+++ brewbuddy/views.py
@@ -7,8 +7,8 @@ from base64 import b64decode
 from base64 import b64encode
 from brewbuddy import app
 
-_CLIENT_ID = os.environ.get('CLIENT_ID')
-_CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+_CLIENT_ID = os.environ['CLIENT_ID']
+_CLIENT_SECRET = os.environ['CLIENT_SECRET']
 
 _SESSION = requests.Session()
 _SESSION.headers.update({'Accept': 'application/json'})
@@ -28,69 +28,99 @@ def hello():
         return authenticate()
     else:
         access_token = flask.session['access_token']
+        scopes = []
+        features = []
         url = _BASE_URL + '/user'
         params = {
             'access_token': access_token,
         }
-        auth_result = _SESSION.get(url, params=params)
-        if auth_result.status_code not in list(range(200, 300)):
+        response = _SESSION.get(url, params=params)
+        if response.status_code not in list(range(200, 300)):
             flask.session['access_token'] = None
             return authenticate()
-        auth_result = auth_result.json()
-        flask.session['auth'] = auth_result
-        return flask.render_template('index.html', auth_result=auth_result)
+        if 'X-OAuth-Scopes' in response.headers:
+            scopes = response.headers['X-OAuth-Scopes'].split(',')
+        auth = response.json()
+        if 'repo' in scopes:
+            url = _BASE_URL + '/repos/%s/%s/contents/%s' % (
+                auth['login'], _REPO, _FILENAME
+            )
+            response = _SESSION.get(url)
+            if response.status_code == 403:
+                message = response.json()['messsage']
+                return flask.render_template('dashboard', message=message)
+            content = b64decode(response.json()['content'])
+            features = json.loads(content)['features']
+        flask.session['auth'] = auth
+        return flask.render_template('dashboard.html', auth=auth,
+                                     features=features)
 
 
 @app.route('/xhr', methods=['POST'])
 def xhr():
+    # By default, assume that this is NOT the initial commit.
     initial = False
-    auth = flask.session['auth']
-    url = (
-        _BASE_URL + '/repos/{}/{}/contents/{}'
-    ).format(auth['login'], _REPO, _FILENAME)
-    data = flask.request.json
+    json_data = flask.request.json
+    # Create the `feature` object. If the geojson file already exists,
+    # then concatenate the `feature` object to the list of `features`.
+    # Otherwise, create a new `FeatureCollection`.
     feature = {
         'type': 'Feature',
         'geometry': {
             'type': 'Point',
-            'coordinates': [data['lng'], data['lat']],
+            'coordinates': [json_data['lng'], json_data['lat']],
         },
         'properties': {
-            'title': data['name'],
+            'title': json_data['title'],
             'marker-size': 'medium',
             'marker-symbol': 'beer',
         }
     }
-    res = _SESSION.get(url)
-    if res.status_code == 404:
+    auth = flask.session['auth']
+    url = _BASE_URL + '/repos/%s/%s/contents/%s' % (
+        auth['login'], _REPO, _FILENAME
+    )
+    response = _SESSION.get(url)
+    if response.status_code == 404:
         initial = True
         content = {
             'type': 'FeatureCollection',
-            'features': []
+            'features': [],
         }
     else:
-        repo = res.json()
+        repo = response.json()
         content = json.loads(b64decode(repo['content']))
-    content['features'].append(feature)
+    titles = [obj['properties']['title']
+              for obj in content['features']]
+    if json_data['title'] not in titles:
+        feature['properties']['description'] = 1
+        content['features'].append(feature)
+    else:
+        # This brewery already exists, so update the counter.
+        index = titles.index(json_data['title'])
+        description = content['features'][index]['properties']['description']
+        description += 1
+        # Insert the `description` key with the updated value.
+        feature['properties']['description'] = description
     content = json.dumps(content, indent=4)
     data = {
         'path': _FILENAME,
         'content': b64encode(content).strip(),
     }
     data['message'] = (
-        'Initial commit' if initial else 'Added some more breweries!'
+        'Initial commit' if initial else json_data['title']
     )
     if not initial:
         data['sha'] = repo['sha']
     data = json.dumps(data)
     access_token = flask.session['access_token']
     headers = {
-        'Authorization': 'Token {}'.format(access_token),
+        'Authorization': 'Token %s' % access_token,
     }
-    res = _SESSION.put(url, data=data, headers=headers)
-    if res.status_code not in list(range(200, 300)):
-        return res.reason, res.status_code
-    return '', res.status_code
+    response = _SESSION.put(url, data=data, headers=headers)
+    if response.status_code not in list(range(200, 300)):
+        return response.reason, response.status_code
+    return flask.jsonify(feature)
 
 
 @app.route('/callback')
